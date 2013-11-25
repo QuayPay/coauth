089bf7d3b00167dcd7b702ae9d8c8aa4646c9f15
Cameron Reeves <camreeves@bitbucket.org>
2013-11-25 15:49:14 +1100

    Remove views

--- lib/coauth.rb
+++ lib/coauth.rb
@@ -8,10 +8,10 @@ require "omniauth-identity"
 require "omniauth-twitter"
 require "omniauth-facebook"
 require "omniauth-openid"
-
-
-old_docs = ::Couchbase::Model::Configuration.design_documents_paths
-::Couchbase::Model::Configuration.design_documents_paths = [File.expand_path(File.join(File.expand_path("../", __FILE__), 'auth'))]
 require "auth/authentication"
-::Auth::Authentication.ensure_design_document!
-::Couchbase::Model::Configuration.design_documents_paths = old_docs
+
+# old_docs = ::Couchbase::Model::Configuration.design_documents_paths
+# ::Couchbase::Model::Configuration.design_documents_paths = [File.expand_path(File.join(File.expand_path("../", __FILE__), 'auth'))]
+
+# ::Auth::Authentication.ensure_design_document!
+# ::Couchbase::Model::Configuration.design_documents_paths = old_docs
