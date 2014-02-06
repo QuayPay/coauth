95d3d6851b5517e00d545efe489c3033688ae29b
Stephen von Takach <steve@cotag.me>
2014-02-06 11:15:46 +1100

    remove auth strategies from coauth
    These are application specific

--- lib/coauth.rb
+++ lib/coauth.rb
@@ -1,13 +1,8 @@
-require "auth/engine"
+require 'auth/engine'
 
 require 'couchbase'
-require "doorkeeper"
-require "open_id/store/couch_store"
-require "omniauth-identity"
-require "omniauth-twitter"
-require "omniauth-facebook"
-require "omniauth-openid"
-require "auth/authentication"
+require 'doorkeeper'
+require 'auth/authentication'
 
 # old_docs = ::Couchbase::Model::Configuration.design_documents_paths
 # ::Couchbase::Model::Configuration.design_documents_paths = [File.expand_path(File.join(File.expand_path("../", __FILE__), 'auth'))]
