ecf28db58f265ba5fecdb0fefd1a967488ec13ca
Stephen von Takach <steve@cotag.me>
2017-03-28 10:41:26 +1100

    add authentication selective stack to coauth

--- lib/coauth/engine.rb
+++ lib/coauth/engine.rb
@@ -2,5 +2,9 @@
 
 module Coauth
     class Engine < ::Rails::Engine
+        config.before_initialize do |app|
+            # Authentication stack is different
+            app.config.middleware.insert_after Rack::Runtime, SelectiveStack
+        end
     end
 end
