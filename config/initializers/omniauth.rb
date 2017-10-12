07cf668b0f63c594d58d2af14dbd2efe04ecdb8b
Stephen von Takach <steve@cotag.me>
2017-10-12 21:48:56 +1100

    configure omniauth to use the rails logger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,9 @@ require 'uv-rays'
 require 'faraday/adapter/libuv'
 Faraday.default_adapter = :libuv
 
+OmniAuth.config.logger = Rails.logger
 Rails.application.config.session_store :cookie_store, key: '_coauth_session'
+
 ::CoauthAuthConfig = proc {
     provider :developer unless Rails.env.production?
     provider :generic_adfs,  name: 'adfs'
