01b6ffd439dddccef66587918c9468dd315bab1c
Stephen von Takach <steve@acaprojects.com>
2018-12-18 11:48:03 +1100

    fix authority check where the user has been deleted.

--- app/controllers/auth/authorities_controller.rb
+++ app/controllers/auth/authorities_controller.rb
@@ -4,12 +4,16 @@ module Auth
     class AuthoritiesController < ActionController::Base
         include UserHelper
         include CurrentAuthorityHelper
-        
+
         def current
             authority = current_authority
             if authority
                 auth = authority.as_json(except: [:created_at, :internals])
-                auth[:session] = signed_in?
+                begin
+                    auth[:session] = signed_in?
+                rescue
+                    auth[:session] = false
+                end
                 auth[:production] = Rails.env.production?
                 render json: auth
             else
