df12721eede1d62f1fe32403df8341e2ac6cdef2
William Le <will@place.technology>
2024-06-17 17:03:30 +0800

    fix(auth): same oauth serializable_hash fix to adfs,ldap

--- app/models/adfs_strat.rb
+++ app/models/adfs_strat.rb
@@ -51,11 +51,12 @@ class AdfsStrat < CouchbaseOrm::Base
         raise 'bad type' unless type.to_s == self.class.design_document
     end
 
-    def serializable_hash(**options)
+    def serializable_hash(options = {}, **opts)
         options = {
-            methods: :type
+          methods: :type
         }.merge!(options)
-        super(**options)
+        options.merge!(opts)
+        super(options)
     end
 
 
