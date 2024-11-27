ee70c2be745e81491e1b1d7448a60246eac49938
camreeves <62260424+camreeves@users.noreply.github.com>
2024-11-27 17:40:33 +1100

    Update token timeout to 2 months

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -62,7 +62,7 @@ Doorkeeper.configure do
     end
 
     # Issue access tokens with refresh token (disabled by default)
-    access_token_expires_in 2.weeks
+    access_token_expires_in 2.months
     use_refresh_token
 
     # Define access token scopes for your provider
